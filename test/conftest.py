15c7c8201cdfe4668fe930d1b83f8ec42bf70613
Azat <azatka6ahgut@gmail.com>
2026-09-16 05:17:41 +0300

    new tests.

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,10 @@
 import uuid
-
+import psycopg2
 import httpx
 import pytest
 
 BASE_URL = "http://localhost:8000"
+
 
 
 @pytest.fixture
@@ -31,3 +32,46 @@ def auth_headers(api_client, unique_email):
     )
     token = response.json()["access_token"]
     return {"Authorization": f"Bearer {token}"}
+@pytest.fixture
+def second_user_headers(api_client):
+    email = f"user_{uuid.uuid4().hex[:8]}@example.com"
+    api_client.post(
+            "/auth/register",
+            json={"email": email, "password": "secret123"},
+        )
+    response = api_client.post(
+            "/auth/login",
+            data={"username": email, "password": "secret123"},
+        )
+    token = response.json()["access_token"]
+    return {"Authorization": f"Bearer {token}"}
+   
+@pytest.fixture
+def admin_headers(api_client):
+    email = f"user_{uuid.uuid4().hex[:8]}@example.com"
+    api_client.post("/auth/register",
+                        json={"email": email, "password": "secret123"},)
+    response = api_client.post(
+                    "/auth/login",
+                    data={"username": email, "password": "secret123"},
+                )
+    token = response.json()["access_token"]
+    connection = psycopg2.connect(
+        "postgresql://shop_user:shop_password@localhost:5432/food_delivery"
+    )
+    cursor = connection.cursor()
+
+    cursor.execute(
+    "UPDATE users SET is_admin = true WHERE email = %s",
+        (email,)
+    )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return {"Authorization": f"Bearer {token}"}
+
+ 
+
+
+
