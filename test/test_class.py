eda64e9b9c11753b934a5b88528e11e38711208e
Azatqa <azatka6ahgut@gmail.com>
2026-09-19 04:44:04 +0300

    add POM

--- test/test_class.py
+++ test/test_class.py
@@ -1,4 +1,5 @@
-from pages import LoginPage
+from pages import LoginPage , CheckoutPage
+from playwright.sync_api import expect
 
 def test_login_via_page_object_with_class(api_client, page, unique_email):
     api_client.post("/auth/register", json={"email": unique_email, "password": "secret123"})
@@ -7,4 +8,37 @@ def test_login_via_page_object_with_class(api_client, page, unique_email):
     login_page.login(unique_email, "secret123")
 
     assert page.url == 'http://localhost:8000/'
+
+def test_full_checkout_flow_via_ui(api_client, page, unique_email):
+    api_client.post("/auth/register", json={"email": unique_email, "password": "secret123"})
+    page.goto('http://localhost:8000/login.html')
+    login_page = LoginPage(page)
+    login_page.login(unique_email, "secret123")
+    first_card = page.locator('[data-testid="product-card"]').first
+    add_button = first_card.locator('[data-testid="add-to-cart-btn"]')
+    add_button.click()    
+    page.goto('http://localhost:8000/cart.html')
+    page.locator('[data-testid="checkout-btn"]').click()
+    pay_page = CheckoutPage(page)
+    pay_page.pay("Test_adres", "4242424242424242", "12/28", "322")
+    page.wait_for_url("http://localhost:8000/orders.html")
+    expect(page.locator('[data-testid="success-message"]')).to_contain_text("оплачен")
+
+
+def test_full_checkout_flow_via_ui(api_client, page, unique_email):
+    api_client.post("/auth/register", json={"email": unique_email, "password": "secret123"})
+    page.goto('http://localhost:8000/login.html')
+    login_page = LoginPage(page)
+    login_page.login(unique_email, "secret123")
+    first_card = page.locator('[data-testid="product-card"]').first
+    add_button = first_card.locator('[data-testid="add-to-cart-btn"]')
+    add_button.click()    
+    page.goto('http://localhost:8000/cart.html')
+    page.locator('[data-testid="checkout-btn"]').click()
+    pay_page = CheckoutPage(page)
+    pay_page.pay("Test_adres", "4000000000000002", "12/28", "322")
+    expect(page.locator('[data-testid="error-message"]')).to_contain_text("отклонена")
+
+
+    
     
